09c887e3fd36ed6f45b29a2ac9adc6546e85f4d4
MZhanbyrtayev@github.com <zhanbyrt@ualberta.ca>
2018-03-27 00:44:35 -0600

    Video Stream capture and save
    This commit manages 4 IP cameras to save video stream in .mp4 format.

---
+++ FrameCaptureApp/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Windows.Forms;
+using Ozeki.Media;
+
+namespace FrameCaptureApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private Button connectBt;
+        private Button disconnectBt;
+        private TextBox secondText;
+        private Label label1;
+        private GroupBox groupBox1;
+        private VideoViewerWF liveViewer;
+        private VideoViewerWF liveViewer2;
+        private VideoViewerWF liveViewer3;
+        private Button startBt;
+        private Button stopBt;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.connectBt = new System.Windows.Forms.Button();
+            this.disconnectBt = new System.Windows.Forms.Button();
+            this.secondText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.liveViewer = new Ozeki.Media.VideoViewerWF();
+            this.liveViewer2 = new Ozeki.Media.VideoViewerWF();
+            this.liveViewer3 = new Ozeki.Media.VideoViewerWF();
+            this.startBt = new System.Windows.Forms.Button();
+            this.stopBt = new System.Windows.Forms.Button();
+            this.liveViewer4 = new Ozeki.Media.VideoViewerWF();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // connectBt
+            // 
+            this.connectBt.Location = new System.Drawing.Point(16, 11);
+            this.connectBt.Margin = new System.Windows.Forms.Padding(4);
+            this.connectBt.Name = "connectBt";
+            this.connectBt.Size = new System.Drawing.Size(175, 28);
+            this.connectBt.TabIndex = 0;
+            this.connectBt.Text = "Connect to web camera";
+            this.connectBt.UseVisualStyleBackColor = true;
+            this.connectBt.Click += new System.EventHandler(this.connectBt_Click);
+            // 
+            // disconnectBt
+            // 
+            this.disconnectBt.Enabled = false;
+            this.disconnectBt.Location = new System.Drawing.Point(931, 11);
+            this.disconnectBt.Margin = new System.Windows.Forms.Padding(4);
+            this.disconnectBt.Name = "disconnectBt";
+            this.disconnectBt.Size = new System.Drawing.Size(225, 28);
+            this.disconnectBt.TabIndex = 2;
+            this.disconnectBt.Text = "Disconnect from web camera";
+            this.disconnectBt.UseVisualStyleBackColor = true;
+            this.disconnectBt.Click += new System.EventHandler(this.disconnectBt_Click);
+            // 
+            // secondText
+            // 
+            this.secondText.Location = new System.Drawing.Point(440, 14);
+            this.secondText.Margin = new System.Windows.Forms.Padding(4);
+            this.secondText.Name = "secondText";
+            this.secondText.Size = new System.Drawing.Size(140, 22);
+            this.secondText.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Seconds between frames";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.liveViewer);
+            this.groupBox1.Controls.Add(this.liveViewer2);
+            this.groupBox1.Location = new System.Drawing.Point(16, 52);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(564, 964);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Live";
+            // 
+            // liveViewer
+            // 
+            this.liveViewer.BackColor = System.Drawing.Color.Black;
+            this.liveViewer.ContextMenuEnabled = true;
+            this.liveViewer.FlipMode = Ozeki.Media.FlipMode.None;
+            this.liveViewer.FrameStretch = Ozeki.Media.FrameStretch.Uniform;
+            this.liveViewer.FullScreenEnabled = true;
+            this.liveViewer.Location = new System.Drawing.Point(6, 531);
+            this.liveViewer.Margin = new System.Windows.Forms.Padding(4);
+            this.liveViewer.Name = "liveViewer";
+            this.liveViewer.RotateAngle = 0;
+            this.liveViewer.Size = new System.Drawing.Size(492, 427);
+            this.liveViewer.TabIndex = 0;
+            this.liveViewer.Text = "liveViewer";
+            // 
+            // liveViewer2
+            // 
+            this.liveViewer2.BackColor = System.Drawing.Color.Black;
+            this.liveViewer2.ContextMenuEnabled = true;
+            this.liveViewer2.FlipMode = Ozeki.Media.FlipMode.None;
+            this.liveViewer2.FrameStretch = Ozeki.Media.FrameStretch.Uniform;
+            this.liveViewer2.FullScreenEnabled = true;
+            this.liveViewer2.Location = new System.Drawing.Point(6, 19);
+            this.liveViewer2.Name = "liveViewer2";
+            this.liveViewer2.RotateAngle = 0;
+            this.liveViewer2.Size = new System.Drawing.Size(492, 479);
+            this.liveViewer2.TabIndex = 0;
+            this.liveViewer2.Text = "liveViewer2";
+            // 
+            // liveViewer3
+            // 
+            this.liveViewer3.BackColor = System.Drawing.Color.Black;
+            this.liveViewer3.ContextMenuEnabled = true;
+            this.liveViewer3.FlipMode = Ozeki.Media.FlipMode.None;
+            this.liveViewer3.FrameStretch = Ozeki.Media.FrameStretch.Uniform;
+            this.liveViewer3.FullScreenEnabled = true;
+            this.liveViewer3.Location = new System.Drawing.Point(7, 19);
+            this.liveViewer3.Name = "liveViewer3";
+            this.liveViewer3.RotateAngle = 0;
+            this.liveViewer3.Size = new System.Drawing.Size(490, 479);
+            this.liveViewer3.TabIndex = 0;
+            this.liveViewer3.Text = "liveViewer3";
+            // 
+            // startBt
+            // 
+            this.startBt.Enabled = false;
+            this.startBt.Location = new System.Drawing.Point(589, 11);
+            this.startBt.Margin = new System.Windows.Forms.Padding(4);
+            this.startBt.Name = "startBt";
+            this.startBt.Size = new System.Drawing.Size(100, 28);
+            this.startBt.TabIndex = 8;
+            this.startBt.Text = "Start";
+            this.startBt.UseVisualStyleBackColor = true;
+            this.startBt.Click += new System.EventHandler(this.startBt_Click);
+            // 
+            // stopBt
+            // 
+            this.stopBt.Enabled = false;
+            this.stopBt.Location = new System.Drawing.Point(697, 11);
+            this.stopBt.Margin = new System.Windows.Forms.Padding(4);
+            this.stopBt.Name = "stopBt";
+            this.stopBt.Size = new System.Drawing.Size(100, 28);
+            this.stopBt.TabIndex = 9;
+            this.stopBt.Text = "Stop";
+            this.stopBt.UseVisualStyleBackColor = true;
+            this.stopBt.Click += new System.EventHandler(this.stopBt_Click);
+            // 
+            // liveViewer4
+            // 
+            this.liveViewer4.BackColor = System.Drawing.Color.Black;
+            this.liveViewer4.ContextMenuEnabled = true;
+            this.liveViewer4.FlipMode = Ozeki.Media.FlipMode.None;
+            this.liveViewer4.FrameStretch = Ozeki.Media.FrameStretch.Uniform;
+            this.liveViewer4.FullScreenEnabled = true;
+            this.liveViewer4.Location = new System.Drawing.Point(8, 531);
+            this.liveViewer4.Margin = new System.Windows.Forms.Padding(4);
+            this.liveViewer4.Name = "liveViewer4";
+            this.liveViewer4.RotateAngle = 0;
+            this.liveViewer4.Size = new System.Drawing.Size(489, 425);
+            this.liveViewer4.TabIndex = 0;
+            this.liveViewer4.Text = "liveViewer4";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.liveViewer4);
+            this.groupBox2.Controls.Add(this.liveViewer3);
+            this.groupBox2.Location = new System.Drawing.Point(589, 52);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(565, 964);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Frame capture";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1163, 1038);
+            this.Controls.Add(this.stopBt);
+            this.Controls.Add(this.startBt);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.secondText);
+            this.Controls.Add(this.disconnectBt);
+            this.Controls.Add(this.connectBt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frame capture";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private VideoViewerWF liveViewer4;
+        private GroupBox groupBox2;
+    }
+}
+
---
+++ FrameCaptureApp/Form1.cs
@@ -0,0 +1,229 @@
+﻿using Ozeki.Camera;
+using Ozeki.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrameCaptureApp
+{
+    public partial class Form1 : Form
+    {
+        private List<FrameCapture> frameCaptures = new List<FrameCapture>();
+
+        private List<IIPCamera> cameras = new List<IIPCamera>();
+        private string[] ips = new string[] { "rtsp://192.168.1.105:554/videoMain",
+                                              "rtsp://192.168.1.106:554/videoMain",
+                                              "rtsp://192.168.1.107:554/videoMain",
+                                              "rtsp://192.168.1.108:554/videoMain" };
+        private List<MediaConnector> connectors = new List<MediaConnector>(); 
+        private List<DrawingImageProvider> imageProviders = new List<DrawingImageProvider>();
+        private List<MPEG4Recorder> recorders = new List<MPEG4Recorder>();
+        //private int _index = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Initialize video component essentials
+            for(int i = 0; i < 4; i++)
+            {
+                imageProviders.Add(new DrawingImageProvider());
+                frameCaptures.Add(new FrameCapture());
+                connectors.Add(new MediaConnector());
+            }
+        }
+
+        private void connectBt_Click(object sender, EventArgs args)
+        {
+            try
+            {
+                disconnectBt.Enabled = true;
+                startBt.Enabled = true;
+
+                //Connect to the cameras
+                for(int i = 0; i < 4; i++)
+                {
+                    cameras.Add(IPCameraFactory.GetCamera(ips[i], "madiZ", "2150183Mkz#"));
+                    cameras[i].CameraStateChanged += OnCameraStateChanged;
+                    cameras[i].CameraErrorOccurred += OnCameraErrorOccurred;
+                    //connect to liveProviders
+                    //connectors[i].Connect(cameras[i].VideoChannel, imageProviders[i]);
+                    //connectors[i].Connect(cameras[i].VideoChannel, frameCaptures[i]);
+                }
+
+
+                //_camera1 = IPCameraFactory.GetCamera("rtsp://192.168.1.105:554/videoMain","madiZ","2150183Mkz#");
+                //_camera1.CameraStateChanged += OnCameraStateChanged;
+                //_camera1.CameraErrorOccurred += OnCameraErrorOccurred;
+                //_connector.Connect(_camera1.VideoChannel, _liveProvider);
+                //_connector.Connect(_camera1.VideoChannel, _capture);
+
+                /*liveViewer.SetImageProvider(imageProviders[0]);
+                liveViewer.Start();
+                liveViewer2.SetImageProvider(imageProviders[1]);
+                liveViewer2.Start();
+                liveViewer3.SetImageProvider(imageProviders[2]);
+                liveViewer3.Start();
+                liveViewer4.SetImageProvider(imageProviders[3]);
+                liveViewer4.Start();*/
+
+                for(int i=0; i<4; i++)
+                {
+                    cameras[i].Start();
+                }
+                connectBt.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnCameraStateChanged(object sender, CameraStateEventArgs e)
+        {
+
+            Console.WriteLine("State Changed to: "+ e.State.ToString());
+        }
+
+        private void OnCameraErrorOccurred(object sender, CameraErrorEventArgs e)
+        {
+            Console.WriteLine("Error occurred: "+ e.ToString());
+            Console.WriteLine("Error occurred: "+ e.Error.ToString());
+        }
+
+        private void disconnectBt_Click(object sender, EventArgs args)
+        {
+            try
+            {
+
+
+                for (int i = 0; i < 4; i++)
+                {
+                    frameCaptures[i].Stop();
+                    frameCaptures[i].OnFrameCaptured -= _capture_onFrameCaptured;
+                    cameras[i].Stop();
+                    connectors[i].Disconnect(cameras[i].VideoChannel, imageProviders[i]);
+                    connectors[i].Disconnect(cameras[i].VideoChannel, frameCaptures[i]);
+                }
+                //_capture.Stop();
+                //_capture4.Stop();
+                //_camera2.Stop();
+                //_camera4.Stop();
+
+                //_connector.Disconnect(_camera2.VideoChannel, _liveProvider);
+                //_connector.Disconnect(_camera2.VideoChannel, _capture);
+
+                //_connector4.Disconnect(_camera4.VideoChannel, _frameCaptureProvider);
+                //_connector4.Disconnect(_camera4.VideoChannel, _capture4);
+
+                //_connector.Disconnect(_capture, _frameCaptureProvider);
+
+
+                liveViewer.Stop();
+                liveViewer2.Stop();
+                liveViewer3.Stop();
+                liveViewer4.Stop();
+                for(int i = 0; i < 4; i++)
+                {
+                    frameCaptures[i].Dispose();
+                }
+                connectBt.Enabled = true;
+                disconnectBt.Enabled = false;
+
+                startBt.Enabled = false;
+                stopBt.Enabled = false;
+            } catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void startBt_Click(object sender, EventArgs args)
+        {
+            try {
+                var sec = int.Parse(secondText.Text);
+                string date = DateTime.Now.Month + "m-" + DateTime.Now.Day + "d-" + DateTime.Now.Hour + "h-" + DateTime.Now.Minute 
+                    +"min-"+DateTime.Now.Second+"sec-"+DateTime.Now.Millisecond+"millis";
+                var defaultPath = "C:\\Users\\madiz\\Desktop\\videoCollection";
+
+                for (int i = 0; i < 4; i++)
+                {
+
+
+                    recorders.Add(new MPEG4Recorder(defaultPath + "\\" + date + "_" + ips[i]));
+                    recorders[i].MultiplexFinished += Mpeg4Recorder_MultiplexFinished;
+                    connectors[i].Connect(cameras[i].AudioChannel, recorders[i].AudioRecorder);
+                    connectors[i].Connect(cameras[i].VideoChannel, recorders[i].VideoRecorder);
+                    // This is listener assignment to check each frames datestamp
+                    // Was added for checking the SDK
+                    //frameCaptures[i].SetInterval(new TimeSpan(0, 0, 0, sec));
+                    //frameCaptures[i].Start();
+                    //frameCaptures[i].OnFrameCaptured += _capture_onFrameCaptured;
+                }
+                //_capture.SetInterval(new TimeSpan(0, 0, 0, sec));
+                //_capture.Start();
+                //_capture.OnFrameCaptured += _capture_onFrameCaptured;
+                stopBt.Enabled = true;
+                startBt.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Mpeg4Recorder_MultiplexFinished(object sender, VoIPEventArgs<bool> e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (recorders[i] != null)
+                {
+                    connectors[i].Disconnect(cameras[i].AudioChannel, recorders[i].AudioRecorder);
+                    connectors[i].Disconnect(cameras[i].VideoChannel, recorders[i].VideoRecorder);
+                    recorders[i].Dispose();
+                }
+            }
+        }
+
+        //This method called whenever the application receives a frame from cameras
+        //Probably need to distinguish between cameras
+        //TODO: Save it to local distro as media file.
+        private void _capture_onFrameCaptured(object sender, Ozeki.Media.Snapshot snapshot)
+        {
+            //var image = snapshot.ToImage();
+            //image.Save("test" + _index + ".jpg");
+            //Console.WriteLine(snapshot.DateStamp.ToString());
+            //Console.WriteLine(snapshot.VideoData.Timestamp.ToString());
+        }
+
+        private void stopBt_Click(object sender, EventArgs args)
+        {
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (recorders[i] != null)
+                {
+                    recorders[i].Multiplex();
+                    connectors[i].Disconnect(cameras[i].AudioChannel, recorders[i].AudioRecorder);
+                    connectors[i].Disconnect(cameras[i].VideoChannel, recorders[i].VideoRecorder);
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                frameCaptures[i].Stop();
+                frameCaptures[i].OnFrameCaptured -= _capture_onFrameCaptured;
+            }
+            //_capture.Stop();
+            //_capture.OnFrameCaptured -= _capture_onFrameCaptured;
+            startBt.Enabled = true;
+            stopBt.Enabled = false;
+        }
+
+
+    }
+}
